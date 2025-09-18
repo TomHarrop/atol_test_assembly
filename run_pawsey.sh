e80ca646c30dc8be022234ea20b2ee8dbb9f33ee
Tom Harrop <twharrop@gmail.com>
2025-09-18 11:05:34 +1000

    taxid168868

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -33,9 +33,8 @@ RESULT_VERSION="v0"
 
 PIPELINE_PARAMS=(
 	"--input" "resources/configs/samplesheet.${RESULT_DIRNAME}.csv"
-	"--outdir" "results/${RESULT_DIRNAME}/results/sanger_tol"
-    "--timestamp" "${RESULT_VERSION}" 
-	"--hifiasm_hic_on"
+	"--outdir" "results/${RESULT_DIRNAME}/results/genomeassembler"
+	"--ont_flye"
 	"-profile" "singularity,pawsey"
 	"-r" "${PIPELINE_VERSION}"
 )
