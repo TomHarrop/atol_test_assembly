25f105d40433d7086f86f7414084d3029c3e8593
Tom Harrop <twharrop@gmail.com>
2025-09-23 11:39:16 +1000

    taxid168868

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -36,7 +36,6 @@ PIPELINE_PARAMS=(
 	"--outdir" "s3://pawsey1132.atol.testassembly/${RESULT_DIRNAME}/results/genomeassembler"
 	"--use_ref" "false"
 	"--lift_annotations" "false"
-	"--busco_db" "s3://pawsey1132.atol.refdata.busco/busco/lineages/hymenoptera_odb10"
 	"--busco_lineage" "hymenoptera_odb10"
 	"-profile" "singularity,pawsey,ont_flye"
 	"-r" "${PIPELINE_VERSION}"
